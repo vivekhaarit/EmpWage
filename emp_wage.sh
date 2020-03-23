f323d761a7d0311ef4790e550e9f43ec4749fdb4
vivek kumar <vivekhaarit@gmail.com>
2020-03-23 19:25:58 +0530

    [vivek]Added:error removed at getworkingHours 0

--- emp_wage.sh
+++ emp_wage.sh
@@ -55,7 +55,7 @@ while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
 	$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	empHours="$( getworkingHours $((RANDOMX3)) )"
+	empHours="$( getworkingHours $((RANDOMX%3)) )"
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
