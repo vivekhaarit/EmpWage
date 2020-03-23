6e45e34d062d2e1d4558fa283bbd16ad074d76c7
vivek kumar <vivekhaarit@gmail.com>
2020-03-23 16:41:08 +0530

    [vivek]Added:checking the attendance

--- emp_wage.sh
+++ emp_wage.sh
@@ -2,4 +2,13 @@
 
 echo "*******welcome to employee wage computation program********"
 
+#checking if the employee is present or not.
+isPresent=$((RANDOM%2));
+echo "checking if emp is present or not"
 
+if [ $isPresent -eq 1 ];
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
