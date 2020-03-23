e184a8f0994787d17c8f8a285e2b4fc5ae93840d
vivek kumar <vivekhaarit@gmail.com>
2020-03-23 19:03:49 +0530

    [vivek]Added:array is created to hold total working days

--- emp_wage.sh
+++ emp_wage.sh
@@ -45,14 +45,21 @@ function getWorkingHours() {
 	esac
 }
 
+function calcDailyWage() {
+	local workHrs $1
+	wage=$(($workHrs*$EMP_RATE_PER_HR))
+	echo $wage
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
 	$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	empHours="$( getworkingHours $((RANDOMX3)) )"
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
 
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
-echo "you'r wage for a month is: "$totalSalary
+totalSalary="$( calcDailyWage StotalWorkHours )"
+echo "Daily Wage " ${empDailyWage[@]}
 
